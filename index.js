8f55bf53261b0f2c4058fe00094055a814eb70f6
chendongdong <564964001@qq.com>
2024-07-08 17:24:04 +0800

    :zap: init

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+const { minify } = require('terser');
+const crypto = require('crypto');
+
+module.exports = () => {
+
+}
